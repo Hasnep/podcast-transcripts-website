3b0d152c1b30b71d9bcf14e35cf93f4084e92c6d
Hannes <h@nnes.dev>
2022-10-12 21:27:05 +0100

    Change data download from using stdlib to fetch module

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,32 +1,44 @@
 import type { GatsbyNode } from "gatsby";
 import type { GetPodcastTranscriptsResult } from "./src/types";
 import path from "path";
-import https from "https";
 import fs from "fs";
+import fetch from "node-fetch";
+import { exit } from "process";
 
-export const onPreInit: GatsbyNode["onPreInit"] = () => {
+export const onPreInit: GatsbyNode["onPreInit"] = async () => {
+  // Get the current environment, defaulting to dev
   const environment = process.env.ENV || "dev";
-  const is_prod = environment == "prod";
+  const isProd = environment == "prod";
 
   // Only download in prod
-  if (is_prod) {
-    console.log("Downloading data.");
-    const url = process.env.DATA_FILE_URL || "";
-    const filePath = "./data/podcast-transcripts.json";
-    // Create the folder if it doesn't exist
-    if (!fs.existsSync("./data")) {
-      fs.mkdirSync("./data");
+  if (isProd) {
+    // Get the URL, raising an error if it was not defined
+    const url = process.env.DATA_FILE_URL;
+    if (typeof url == "undefined") {
+      console.error("Environment variable 'DATA_FILE_URL' was not set.");
+      exit(1);
     }
-    const file = fs.createWriteStream(filePath);
-    https.get(url, (response) => {
-      response.pipe(file);
-      file
-        .on("finish", () => file.close())
-        .on("error", (err) => fs.unlink(filePath, (err) => {}));
+
+    // Create the folder if it doesn't exist
+    const dataFolder = path.resolve(path.join(".", "data"));
+    if (!fs.existsSync(dataFolder)) {
+      console.log(`Creating data folder at '${dataFolder}'.`);
+      fs.mkdirSync(dataFolder);
+    }
+
+    // Download the file
+    const filePath = path.join(dataFolder, "podcast-transcripts.json");
+    console.log(`Downloading data to ${filePath}.`);
+    const response = await fetch(url);
+    const data = await response.json();
+    fs.writeFile(filePath, JSON.stringify(data), (err) => {
+      if (err) {
+        console.error(err);
+        exit(1);
+      } else {
+        console.log("Downloaded data.");
+      }
     });
-    console.log("Downloaded data.");
-  } else {
-    console.log("Skipping download.");
   }
 };
 
