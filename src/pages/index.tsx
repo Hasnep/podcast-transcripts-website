effbffd5b3a47bbad41a66a74e65dd0150ec585d
Hannes <h@nnes.dev>
2022-10-29 23:47:42 +0100

    Remove not very useful link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,8 @@ const IndexPage = ({
     <h1>{siteTitle}</h1>
     <h2>About</h2>
     <p>
-      A an <a href={"#source-code"}>open source</a> website with automatically
-      generated transcripts of podcasts. Uses{" "}
+      A an open source website with automatically generated transcripts of
+      podcasts. Uses{" "}
       <a href={"https://openai.com/blog/whisper/"}>OpenAI's Whisper</a> for
       transcription.
     </p>
