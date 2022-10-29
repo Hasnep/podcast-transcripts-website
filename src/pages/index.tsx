20cd4bdace3b66c03e49735c2bbac80646bebb9b
Hannes <h@nnes.dev>
2022-10-29 22:33:34 +0100

    Improve index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,22 @@ import { PageQueryResult } from "../types";
 
 const IndexPage = ({
   data: {
+    site: {
+      siteMetadata: { siteTitle },
+    },
     dataJson: { podcasts },
   },
 }: PageProps<PageQueryResult>) => (
   <main>
-    <h1>hi</h1>
+    <h1>{siteTitle}</h1>
+    <h2>About</h2>
+    <p>
+      A an <a href={"#source-code"}>open source</a> website with automatically
+      generated transcripts of podcasts. Uses{" "}
+      <a href={"https://openai.com/blog/whisper/"}>OpenAI's Whisper</a> for
+      transcription.
+    </p>
+    <h2>Podcasts</h2>
     <ul>
       {podcasts
         .sort((a, b) => compareStrings(a.podcastTitle, b.podcastTitle))
@@ -22,6 +33,47 @@ const IndexPage = ({
             </Link>
           </li>
         ))}
+    </ul>
+    <h2 id={"source-code"}>Source Code</h2>
+    This website and the software that transcribe the podcasts are completely
+    free and open source. The copyright of the podcasts are owned by their
+    original creators.
+    <ul>
+      <li>
+        <a href={"https://github.com/podcast-transcripts/podcast-transcriber"}>
+          Source code
+        </a>{" "}
+        for the podcast scraper, downloader and transcriber —{" "}
+        <a
+          href={
+            "https://github.com/podcast-transcripts/podcast-transcripts-website/blob/main/LICENCE"
+          }
+        >
+          GPL-3
+        </a>
+      </li>
+      <li>
+        <a
+          href={
+            "https://github.com/podcast-transcripts/podcast-transcripts-website"
+          }
+        >
+          Source code
+        </a>{" "}
+        for the website you're reading now —{" "}
+        <a
+          href={
+            "https://github.com/podcast-transcripts/podcast-transcripts-website/blob/main/LICENCE"
+          }
+        >
+          GPL-3
+        </a>
+      </li>
+      <li>
+        <a href={"https://github.com/openai/whisper"}>Source code</a> for
+        OpenAI's Whisper which is used for speech-to-text —{" "}
+        <a href={"https://github.com/openai/whisper/blob/main/LICENSE"}>MIT</a>
+      </li>
     </ul>
   </main>
 );
