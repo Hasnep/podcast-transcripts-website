910a11c6fdc8be2b60398e301f216bffe1ec32b5
Hannes <h@nnes.dev>
2022-10-29 18:24:24 +0100

    Remove data download step from preinit step

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,46 +1,6 @@
 import type { GatsbyNode } from "gatsby";
 import type { GetPodcastTranscriptsResult } from "./src/types";
 import path from "path";
-import fs from "fs";
-import fetch from "node-fetch";
-import { exit } from "process";
-
-export const onPreInit: GatsbyNode["onPreInit"] = async () => {
-  // Get the current environment, defaulting to dev
-  const environment = process.env.ENV || "dev";
-  const isProd = environment == "prod";
-
-  // Only download in prod
-  if (isProd) {
-    // Get the URL, raising an error if it was not defined
-    const url = process.env.DATA_FILE_URL;
-    if (typeof url == "undefined") {
-      console.error("Environment variable 'DATA_FILE_URL' was not set.");
-      exit(1);
-    }
-
-    // Create the folder if it doesn't exist
-    const dataFolder = path.resolve(path.join(".", "data"));
-    if (!fs.existsSync(dataFolder)) {
-      console.log(`Creating data folder at '${dataFolder}'.`);
-      fs.mkdirSync(dataFolder);
-    }
-
-    // Download the file
-    const filePath = path.join(dataFolder, "podcast-transcripts.json");
-    console.log(`Downloading data to ${filePath}.`);
-    const response = await fetch(url);
-    const data = await response.json();
-    fs.writeFile(filePath, JSON.stringify(data), (err) => {
-      if (err) {
-        console.error(err);
-        exit(1);
-      } else {
-        console.log("Downloaded data.");
-      }
-    });
-  }
-};
 
 export const createPages: GatsbyNode["createPages"] = async ({
   actions: { createPage },
